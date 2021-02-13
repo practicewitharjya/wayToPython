6c159b5fb7c6ceb3d0cb43a09816db65bda457a5
practicewitharjya <practicewitharjya@gmail.com>
2021-02-13 12:13:11 +0530

    Snake Game Day2

--- snakeGame/main.py
+++ snakeGame/main.py
@@ -1,29 +1,28 @@
 from turtle import Turtle,Screen
+from snakeGame.snake import Snake
+import time
+
 
 screen = Screen()
-screen.setup(width=600,height=600)
-screen.bgcolor('black')
-screen.title("Welcome To Snake Game")
-starting_position = [(0, 0), (-20, 0), (-40, 0)]
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Welcome To My Snake Game")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 game_is_on = True
-segments = []
-
-for position in starting_position:
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(position)
-    segments.append(new_segment)
-
-
 while game_is_on:
-    for seg in segments:
-        seg.forward(20)
+    screen.update()
+    time.sleep(0.1)
 
-
-
-
-
+    snake.move()
 
 
 screen.exitonclick()
